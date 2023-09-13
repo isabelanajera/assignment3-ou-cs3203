3571abf6efb9508f88c41e2ceed784f3013daacd
isabela <isabela.najera@ou.edu>
2023-09-13 15:02:30 -0500

    first

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3{
+int sum(int [] array){
+
+   
+    int sum = 0;
+    for(int i = 0; i < array.length; i ++){
+        sum+=array[i];
+    }
+    return sum;
+}
+public static void main(String args[]){}
+}
