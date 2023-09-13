17aaa0867184ec6913759029fc3c7fa56c359a87
isabela <isabela.najera@ou.edu>
2023-09-13 15:05:08 -0500

    third

--- Assignment3.java
+++ Assignment3.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Assignment3{
 int sum(int [] array){
 
@@ -19,5 +19,21 @@ int product(int [] array){
     }
     return product;
 }
-public static void main(String args[]){}
+public static void main(String args[]){Assignment3 assignment3 = new Assignment3();
+    Scanner keyboard = new Scanner(System.in);
+    
+     System.out.println("Enter size");
+    int size = keyboard.nextInt();
+    int [] array = new int[size];
+    System.out.println("Enter numbers");
+    for(int i = 0; i < size; i ++){
+    array[i] = keyboard.nextInt();
+    }
+    
+    int sum = assignment3.sum(array);
+    System.out.println("Sum " + sum);
+    int product = assignment3.product(array);
+    System.out.println("Product " + product);
+    
+    }
 }
